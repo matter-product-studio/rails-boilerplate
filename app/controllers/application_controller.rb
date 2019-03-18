66e7b69c0780afb2b8d25589a238f525e947fe3c
Elijah Murray <elijahmurray@gmail.com>
2019-03-18 10:32:14 -0400

    add code to easily sanitize parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  #Enable this to allow name to be in a permissable parameter for editing and signup through Devise
+  #before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
