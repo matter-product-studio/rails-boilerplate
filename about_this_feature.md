22b463ddd49352a04de0b54904a24ecb2e70525d
Elijah Murray <elijahmurray@gmail.com>
2020-04-21 10:13:36 -0400

    wip; not working

---
+++ about_this_feature.md
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/24235805/rails-4-how-do-i-create-a-custom-404-page-that-uses-the-asset-pipeline
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   root 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # error pages
+  %w( 404).each do |code|
+    get code, :to => "errors#show", :code => code
+  end
 end
